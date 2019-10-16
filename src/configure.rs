941d3397e1c6889de8e8f704a4eb4c5671acdccc
ekuinox <depkey@me.com>
2019-10-16 11:20:48 +0900

    設定ファイルから読み込めるように

---
+++ src/configure.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Target {
+    pub domain: String,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Configures {
+    pub targets: HashMap<String, Vec<Target>>,
+}
+
+impl Configures {
+    pub fn import_from_file<'a>(path: &'a str) -> Result<Configures, serde_json::Error> {
+        serde_json::from_str::<Configures>(read_to_string(path).unwrap().as_str())
+    }
+}
